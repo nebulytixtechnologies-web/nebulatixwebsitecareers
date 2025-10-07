501f3be71e9e90210b28b6850fcc45ff14606bd3
kottemanasa <Kottemanasa018@gmail.com>
2025-10-07 15:58:59 +0530

    Entity Creation

--- NebulytixCareersPage/src/main/java/com/neb/entity/EntityClass.java
+++
@@ -1,7 +0,0 @@
-package com.neb.entity;
-
-public class EntityClass {
-	private String name;
-	private String rollNumber;
-
-}
---
+++ NebulytixCareersPage/src/main/java/com/neb/entity/InternFormData.java
@@ -0,0 +1,33 @@
+package com.neb.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name="careerform")
+public class InternFormData {
+	
+	@SequenceGenerator(name="carre1",sequenceName = "seq1",initialValue = 1,allocationSize = 1)
+	@GeneratedValue(generator = "seq1",strategy =GenerationType.SEQUENCE )
+	@Id
+	private Integer cid;
+	@Column(length = 20)
+	private String cname;
+	@Column(length = 20)
+	private String email;
+    private Long phoneno;
+    private String resumePath;
+    
+    
+}
