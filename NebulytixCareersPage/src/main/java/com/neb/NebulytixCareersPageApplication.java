c3b9681db8869142fdff84a3278e497f278ff433
USER <USER@DESKTOP-A7RGBR5.mshome.net>
2025-10-07 14:07:32 +0530

    Git empty project initail commit

---
+++ NebulytixCareersPage/src/main/java/com/neb/NebulytixCareersPageApplication.java
@@ -0,0 +1,13 @@
+package com.neb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NebulytixCareersPageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NebulytixCareersPageApplication.class, args);
+	}
+
+}
